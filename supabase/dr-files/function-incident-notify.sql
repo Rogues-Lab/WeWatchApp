4ed3d64036eae812c34c3443a82fe1c7fc550794
Darren Rogan Personal <darrenrogan@gmail.com>
2023-10-07 11:41:13 +1000

    updated db functions

--- supabase/dr-files/function-incident-notify.sql
+++ supabase/dr-files/function-incident-notify.sql
@@ -11,20 +11,47 @@ CREATE OR REPLACE FUNCTION geo_users(x numeric, y numeric, distance int)
 $$ language sql;
 
 -- Triggers need to be ROW level, not STATEMENT level
-CREATE OR REPLACE FUNCTION public.notify_incident()
- RETURNS trigger
- LANGUAGE plpgsql
- SECURITY DEFINER
-AS $$
-begin
-    insert into public.notifications(user_id, message, mode, object_type, object_id)
-    select gu.id, new.name, 'create', 'incidents', new.id
-    FROM geo_users(new.longitude, new.latitude, 1000) gu;
+-- CREATE OR REPLACE FUNCTION public.notify_incident()
+--  RETURNS trigger
+--  LANGUAGE plpgsql
+--  SECURITY DEFINER
+-- AS $$
+-- begin
+--     insert into public.notifications(user_id, message, mode, object_type, object_id)
+--     select gu.id, new.name, 'create', 'incidents', new.id
+--     FROM geo_users(new.longitude, new.latitude, 1000) gu;
 
- return new;
-end;
-$$
+--  return new;
+-- end;
+-- $$
 
     -- insert into public.notifications(user_id, message, mode, object_type, object_id)
     -- select (gu.id, new.name, 'create', 'incidents',new.id)
     -- FROM public.geo_users(new.longitude, new.latitude, 100000000000) gu;
+
+CREATE OR REPLACE FUNCTION public.notify_incident()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+
+    RAISE NOTICE 'New incident created with name %', new.name;
+    
+    insert into public.notifications(user_id, message, mode, object_type, object_id)
+    select gu.id, new.name, 'create', 'incidents', new.id
+    FROM geo_users(new.longitude, new.latitude, 1000) gu;
+
+    PERFORM net.http_get('https://app.wewatchapp.com/api/notifier/incident/'||new.id|| '?API_ROUTE_SECRET=dsfersfsdgwrgw5gwer5tedhre5hjf6tjk74ye543655645DGFN5YEHMHK57yge55rye4Y43q4344234tefq4wtserwgewefdfwssrtr'
+    );
+
+ return new;
+end;
+$function$
+
+
+CREATE TRIGGER incident_insert_trigger
+AFTER INSERT ON incidents
+FOR EACH ROW
+EXECUTE FUNCTION notify_incident();
+  
---
+++ supabase/dr-files/function-reports-notify.sql
@@ -0,0 +1,23 @@
+  
+CREATE OR REPLACE FUNCTION public.notify_report()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+
+    RAISE NOTICE 'New report created with name %', new.name;
+
+
+    PERFORM net.http_get('https://app.wewatchapp.com/api/notifier/report/'||new.id|| '?API_ROUTE_SECRET=dsfersfsdgwrgw5gwer5tedhre5hjf6tjk74ye543655645DGFN5YEHMHK57yge55rye4Y43q4344234tefq4wtserwgewefdfwssrtr'
+    );
+
+ return new;
+end;
+$function$
+
+
+CREATE TRIGGER reports_insert_trigger
+AFTER INSERT ON reports
+FOR EACH ROW
+EXECUTE FUNCTION notify_report();
