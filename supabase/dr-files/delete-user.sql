18c43a4d39cb3fdf62352fdf47422ba4d7c41a87
Darren Rogan Personal <darrenrogan@gmail.com>
2023-11-04 09:06:56 +1000

    remove user psql

--- supabase/dr-files/delete-user.sql
+++ supabase/dr-files/delete-user.sql
@@ -1,36 +1,16 @@
-UPDATE messages SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-UPDATE files SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-UPDATE incidents SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-UPDATE reports SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM notifications WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM chat_members WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM chats WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM reports WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM incidents WHERE user_id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM users WHERE id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
-DELETE FROM auth.users WHERE id IN ('c1002aac-8f26-4ecf-a949-0c133e01a4c7');
--- delete from auth.audit_log_entries;
-commit;
-
-
-
-
-CREATE OR REPLACE FUNCTION remove_user(user_id uuid)
-RETURNS BOOLEAN AS $$
+create
+or replace function removeUser (userId uuid) returns VOID as $$
 BEGIN
-  -- Move messages to a different user (so we don't lose them)
-  UPDATE messages SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id = $1;
-  UPDATE files SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id = $1;
-  UPDATE incidents SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id = $1;
-  UPDATE reports SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id = $1;
+  UPDATE messages SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN (userId);
+  UPDATE files SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN (userId);
+  UPDATE incidents SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN (userId);
+  UPDATE reports SET completed = true WHERE user_id IN (userId);
+  UPDATE reports SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN (userId);
+  UPDATE chats SET user_id = 'f0d7d5d9-3b3d-4c3f-8d8c-6d5f9b3d9b6c' WHERE user_id IN (userId);
 
-  -- Delete related data from other tables
-  DELETE FROM notifications WHERE user_id = $1;
-  DELETE FROM chat_members WHERE user_id = $1;
-
-  -- Delete user from the 'users' table
-  DELETE FROM users WHERE id = $1;
-
-  RETURN TRUE;
+  DELETE FROM notifications WHERE user_id IN (userId);
+  DELETE FROM chat_members WHERE user_id IN (userId);
+  DELETE FROM users WHERE id IN (userId);
+  DELETE FROM auth.users WHERE id IN (userId);
 END;
-$$ LANGUAGE plpgsql;
+$$ language plpgsql;
